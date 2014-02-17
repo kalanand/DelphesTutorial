28ad0e2c4b0949d6d734359c0b52b4772f4561b0
Ben@nbay04 <benwu@fnal.gov>
2014-02-16 18:10:57 -0600

    Update HATS for students
    1. Polished the DelHATS.cc as discussed
    ==> No the best naming for the histograms yet. Need further polishing
    2. Remove the answer from the master branch

--- HATS/DelHATS.cc
+++ HATS/DelHATS.cc
@@ -2,15 +2,14 @@
 // 
 //       Filename:  DelHATS.cc
 // 
-//    Description:  
+//    Description:  An example code for Delphes exercise
 // 
 //        Version:  1.0
 //        Created:  02/11/2014 01:44:39 PM
 //       Compiler:  g++ -std=c++11
 // 
-//         Author:  Zhenbin Wu (benwu)
-//          Email:  benwu@fnal.gov
-//        Company:  Baylor University, CDF@FNAL, CMS@LPC
+//         Author:  Zhenbin Wu, John Stupak
+//        Company:  HATS@LPC
 // 
 // ===========================================================================
 
@@ -38,32 +37,79 @@
 // ===========================================================================
 int main ( int argc, char *argv[] )
 {
-  if(argc != 2)
+  if(argc != 3)
   {
-    std::cout << " Usage: DelHATS input_file" << std::endl;
+    std::cout << " Usage: DelHATS input_file output_file" << std::endl;
     std::cout << " input_file - input file in ROOT format ('Delphes' tree)," << std::endl;
+    std::cout << " output_file - output file in ROOT format" << std::endl;
     return EXIT_FAILURE;
   }
 
   // Getting the input filename
-  const std::string inputFile  = argv[1];
+  const std::string inputFile_name  = argv[1];
+  const std::string outputFile_name = argv[2];
 
   // Create chain of root trees
   TChain chain("Delphes");
-  chain.Add(inputFile.c_str());
+  chain.Add(inputFile_name.c_str());
 
-  // Keep the root window open
-  TApplication *app = new TApplication("DelHATS", &argc, argv);
-  
+  // Create the output file
+  TFile outputfile(outputFile_name.c_str(), "RECREATE");
+
   // Create object of class ExRootTreeReader
   ExRootTreeReader *treeReader = new ExRootTreeReader(&chain);
   Long64_t numberOfEntries = treeReader->GetEntries();
   
   // Get pointers to branches used in this analysis
-  TClonesArray *branchJet = treeReader->UseBranch("Jet");
+  TClonesArray *branchJet        = treeReader->UseBranch("Jet");
+  TClonesArray *branchGenJet     = treeReader->UseBranch("GenJet");
+  TClonesArray *branchElectron   = treeReader->UseBranch("Electron");
+  TClonesArray *branchMuon       = treeReader->UseBranch("Muon");
+  TClonesArray *branchPhoton     = treeReader->UseBranch("Photon");
+  TClonesArray *branchMet        = treeReader->UseBranch("MissingET");
+  TClonesArray *branchParticle   = treeReader->UseBranch("Particle");
   
   // Book histograms
-  TH1 *histJetPT = new TH1F("jet_pt", "jet P_{T}", 100, 0.0, 100.0);
+//----------------------------------------------------------------------------
+//  Example 
+//---------------------------------------------------------------------------
+  TH1 *histJetPT = new TH1F("jet_pt", "jet P_{T}", 500, 0.0, 1000);
+
+//----------------------------------------------------------------------------
+//  Lepton Efficiency exercise
+//----------------------------------------------------------------------------
+
+  //TH1 *histGenLepEta      = new TH1F("histGenLepEta ", "GenLepton Eta", 120, -6, 6);
+  //TH1 *histMatchGenLepEta = new TH1F("histMatchGenLepEta", "Matched GenLepton Eta", 120, -6, 6);
+  //TH1 *histLepEffEta      = new TH1F("histLepEffEta", "Lepton Efficiency", 120, -6, 6);
+
+
+//----------------------------------------------------------------------------
+//  JEtMET execise
+//----------------------------------------------------------------------------
+  //TH1 *histJetPT = new TH1F("jet_pt", "jet P_{T}", 500, 0.0, 1000);
+  //TH1 *histJetEta = new TH1F("jet_eta", "jet Eta", 120, -6, 6);
+
+  ////  MET and MET resolution
+  //TH1 *histMET = new TH1F("MET", "MET", 100, 0.0, 1000);
+  //TH1 *histMET_X = new TH1F("MET_X", "MET_X", 300, -300.0, 300.0);
+  //TH1 *histMET_Y = new TH1F("MET_Y", "MET_Y", 300, -300.0, 300.0);
+
+  //// MHT and MHT resolution
+  //TH1 *histMHT = new TH1F("MHT", "MHT", 100, 0.0, 1000);
+  //TH1 *histMHT_X = new TH1F("MHT_X", "MHT_X", 300, -300.0, 300.0);
+  //TH1 *histMHT_Y = new TH1F("MHT_Y", "MHT_Y", 300, -300.0, 300.0);
+
+  //// Jet response: approximate by TProfile
+  //TProfile *histJetResPT = new TProfile("histJetResPT ", 
+      //"Jet response as a fucntion of GenJet Pt", 100, 0, 500);
+  //TProfile *histJetResEta = new TProfile("histJetResEta ", 
+      //"Jet response as a function of GenJet Eta", 100, -5, 5);
+
+  //// Jet resolution for Eta (0, 2.5, 4, 5) and Pt (30, 50);
+  //TH1 *histJetEta1 = new TH1F("histJetEta1 ", "Jet Resolution with eta (0, 2.5)", 200, 4, 4);
+  //TH1 *histJetEta2 = new TH1F("histJetEta2 ", "Jet Resolution with eta (2.5, 4)", 200, 4, 4);
+  //TH1 *histJetEta3 = new TH1F("histJetEta3 ", "Jet Resolution with eta (4, 5)", 200, 4, 4);
 
 //----------------------------------------------------------------------------
 //   Loop over all events
@@ -87,10 +133,24 @@ int main ( int argc, char *argv[] )
     }
   }
 
-  // Show resulting histograms
-  histJetPT->Draw();
+  // Saving resulting histograms
+  histJetPT->Write();
+  //histGenLepEta->Write();
+  //histMatchGenLepEta->Write();
+  //histLepEffEta->Write();
+  //histJetEta->Write();
+  //histMET->Write();
+  //histMET_X->Write();
+  //histMET_Y->Write();
+  //histMHT->Write();
+  //histMHT_X->Write();
+  //histMHT_Y->Write();
+  //histJetResPT->Write();
+  //histJetResEta->Write();
+  //histJetEta1->Write();
+  //histJetEta2->Write();
+  //histJetEta3->Write();
 
-  app->Run();
-
+  outputfile.Close();
   return EXIT_SUCCESS;
 }				// ----------  end of function main  ----------
